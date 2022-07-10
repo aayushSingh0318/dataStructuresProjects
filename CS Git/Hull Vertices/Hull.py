867afae959219358b327ef1c2852a8a2f71a0d25
Aayush Singh <98490572+skadooshm4n@users.noreply.github.com>
2022-07-10 14:04:08 -0500

    Update Hull.py

--- CS Git/Hull Vertices/Hull.py
+++ CS Git/Hull Vertices/Hull.py
@@ -1,22 +1,4 @@
-#  File: Hull.py
 
-#  Description:
-
-#  Student Name:
-
-#  Student UT EID:
-
-#  Partner Name:
-
-#  Partner UT EID:
-
-#  Course Name: CS 313E
-
-#  Unique Number: 
-
-#  Date Created:
-
-#  Date Last Modified:
 import sys
 import math
 
