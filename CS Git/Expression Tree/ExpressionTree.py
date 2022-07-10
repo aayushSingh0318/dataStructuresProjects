5e19f7b9e06a0c9043539d14c7816dc6cb9a18c3
Aayush Singh <98490572+skadooshm4n@users.noreply.github.com>
2022-07-10 13:33:22 -0500

    Update ExpressionTree.py

--- CS Git/Expression Tree/ExpressionTree.py
+++ CS Git/Expression Tree/ExpressionTree.py
@@ -2,21 +2,6 @@
 
 #  Description: This is an expression tree. 
 
-#  Student Name: Aayush Singh
-
-#  Student UT EID:  as92488
-
-#  Partner Name: Danny Xie
-
-#  Partner UT EID: dax56
-
-#  Course Name: CS 313E
-
-#  Unique Number: 51120
-
-#  Date Created:4/13/2022
-
-#  Date Last Modified: 4/13/2022
 
 import sys
 
